606e07f5239a9fa4ea6c1389f520c9d3ec8df32f
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-01-13 19:04:47 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolVoBallovConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float scoreProgramming = 65.5F;
+            float scoreMath = 76.5F;
+            float scorePhysics = 57.5F;
+            float summ = (scoreProgramming + scoreMath + scorePhysics);
+            float arithmeticMean = summ / 3;
+
+            Console.WriteLine(summ);
+            Console.WriteLine(arithmeticMean);
+            
+            Console.ReadKey();
+        }
+    }
+}
